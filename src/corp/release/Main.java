341a80e7cca2907f0f2386d08f8b191cf3158c0e
Daniel Bay <102850382+Daniel-Bay@users.noreply.github.com>
2022-04-05 08:51:25 -0700

    Update Main.java
    Change from Git directly

--- src/corp/release/Main.java
+++ src/corp/release/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, QA.Guru!!!");
         System.out.println("Just one more line!");
+        
+    public static void another () {
+        System.out.println("Another method");
     }
 }
