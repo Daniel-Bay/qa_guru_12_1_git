14b2eaa4bb936c032db2660ac4c09540d23cfa86
Daniel Bay <release.corp@yandex.com>
2022-04-05 08:50:15 -0700

    Added a new line with slow branch

--- src/corp/release/Main.java
+++ src/corp/release/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, QA.Guru!!!");
         System.out.println("Just one more line!");
+        System.out.println("slow branch");
     }
 }
